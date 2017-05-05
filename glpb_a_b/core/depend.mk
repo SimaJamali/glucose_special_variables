011d1535e07ba6193be747d8f216c46c42153fe2
SimaJamali <sja88@sfu.ca>
2017-05-04 19:56:52 -0700

    Add files via upload

---
+++ glpb_a_b/core/depend.mk
@@ -0,0 +1,34 @@
+/local-scratch/sima/Glucose/modified_solver/core/Main.o /local-scratch/sima/Glucose/modified_solver/core/Main.or /local-scratch/sima/Glucose/modified_solver/core/Main.od /local-scratch/sima/Glucose/modified_solver/core/Main.op: /local-scratch/sima/Glucose/modified_solver/core/Main.cc \
+ /local-scratch/sima/Glucose/modified_solver/utils/System.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/IntTypes.h \
+ /local-scratch/sima/Glucose/modified_solver/utils/ParseUtils.h \
+ /local-scratch/sima/Glucose/modified_solver/utils/Options.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Vec.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/XAlloc.h \
+ /local-scratch/sima/Glucose/modified_solver/core/Dimacs.h \
+ /local-scratch/sima/Glucose/modified_solver/core/SolverTypes.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Alg.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Map.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Alloc.h \
+ /local-scratch/sima/Glucose/modified_solver/core/Solver.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Heap.h \
+ /local-scratch/sima/Glucose/modified_solver/core/BoundedQueue.h \
+ /local-scratch/sima/Glucose/modified_solver/core/Constants.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Clone.h
+/local-scratch/sima/Glucose/modified_solver/core/Solver.o /local-scratch/sima/Glucose/modified_solver/core/Solver.or /local-scratch/sima/Glucose/modified_solver/core/Solver.od /local-scratch/sima/Glucose/modified_solver/core/Solver.op: /local-scratch/sima/Glucose/modified_solver/core/Solver.cc \
+ /local-scratch/sima/Glucose/modified_solver/utils/System.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/IntTypes.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Sort.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Vec.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/XAlloc.h \
+ /local-scratch/sima/Glucose/modified_solver/core/Solver.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Heap.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Alg.h \
+ /local-scratch/sima/Glucose/modified_solver/utils/Options.h \
+ /local-scratch/sima/Glucose/modified_solver/utils/ParseUtils.h \
+ /local-scratch/sima/Glucose/modified_solver/core/SolverTypes.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Map.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Alloc.h \
+ /local-scratch/sima/Glucose/modified_solver/core/BoundedQueue.h \
+ /local-scratch/sima/Glucose/modified_solver/core/Constants.h \
+ /local-scratch/sima/Glucose/modified_solver/mtl/Clone.h
