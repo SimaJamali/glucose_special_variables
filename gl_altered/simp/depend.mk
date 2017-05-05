dcdec38822130a5faced863056a31ff03f84bfac
SimaJamali <sja88@sfu.ca>
2017-05-04 19:54:03 -0700

    Add files via upload

---
+++ gl_altered/simp/depend.mk
@@ -0,0 +1,40 @@
+/local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/Main.o /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/Main.or /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/Main.od /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/Main.op: \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/Main.cc \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/System.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/IntTypes.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/ParseUtils.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/Options.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Vec.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/XAlloc.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/Dimacs.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/SolverTypes.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Alg.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Map.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Alloc.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../simp/SimpSolver.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Queue.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/Solver.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Heap.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/BoundedQueue.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/Constants.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Clone.h
+/local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/SimpSolver.o /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/SimpSolver.or /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/SimpSolver.od /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/SimpSolver.op: \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/SimpSolver.cc \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Sort.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Vec.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/IntTypes.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/XAlloc.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../simp/SimpSolver.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Queue.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/Solver.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Heap.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Alg.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/Options.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/ParseUtils.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/SolverTypes.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Map.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Alloc.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/BoundedQueue.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../core/Constants.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../mtl/Clone.h \
+ /local-scratch/sima/Glucose/results/Complete/27.3.conference-new-best-solvers-all/solvers-codes/redundant/simp/../utils/System.h
